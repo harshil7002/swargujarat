01b09015e604d33d0e04f2e0abefd658bc68f6cc
harshil7002 <jethvaharshil1@gmail.com>
2026-08-16 12:19:20 +0530

    Delete Swar Gujarat directory

--- Swar Gujarat/css/Swar Gujarat/functions/api/logout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost() {
-    return new Response(JSON.stringify({ success: true }), {
-        status: 200,
-        headers: {
-            "Content-Type": "application/json",
-            "Set-Cookie": `swar_admin_session=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Lax`
-        }
-    });
-}
